7dece671b0c97baa891097078c59644d76cfdc49
raspmldlgycchang <raspmldlgycchang8753@naver.com>
2022-05-03 18:10:22 +0900

    ValueClient Proj added

---
+++ CSocketCameraPhotoProj02/ValueClient/MyClient.cpp
@@ -0,0 +1,30 @@
+﻿// MyClient.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "ValueClient.h"
+#include "MyClient.h"
+
+
+// MyClient
+
+MyClient::MyClient()
+{
+}
+
+MyClient::~MyClient()
+{
+}
+
+
+// MyClient 멤버 함수
+
+
+void MyClient::OnReceive(int nErrorCode)
+{
+
+	char str[1024];
+	Receive(str, 1024);
+
+	CSocket::OnReceive(nErrorCode);
+}
---
+++ CSocketCameraPhotoProj02/ValueClient/ValueClient.h
@@ -0,0 +1,32 @@
+﻿
+// ValueClient.h: PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CValueClientApp:
+// 이 클래스의 구현에 대해서는 ValueClient.cpp을(를) 참조하세요.
+//
+
+class CValueClientApp : public CWinApp
+{
+public:
+	CValueClientApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CValueClientApp theApp;
